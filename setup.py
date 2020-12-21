4b1c638d73196b153690a5adbe6fa0da781152e9
Julien Finet <julien.finet@kitware.com>
2020-12-21 07:58:26 +0100

    ENH: Add setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdf-aggregator",
+    version="0.0.1",
+    author="Julien Finet",
+    author_email="julien.finet@kitware.com",
+    description="Aggregate account PDF statements into JSON and visualize aggregated financial data as timeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/finetjul/pdf-aggregator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: OS Independent",
+    ],
+    keywords='pdf aggregate extract banking financial statement',
+    # Define an executable calls dicom-anonymizer from a specific file
+    entry_points={
+        'console_scripts': [
+            'pdf-aggregator = aggregator.aggregate:main'
+            'pdf-plot = aggregator.plot:main'
+        ]
+    },
+    python_requires='>=3.6',
+)
